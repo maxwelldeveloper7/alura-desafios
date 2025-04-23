23af310ae913d60f7f21922dda177f4a135ecdda
Maxwell de Oliveira Chaves <maxwellchaves1844@gmail.com>
2025-04-23 12:38:52 -0300

    concluído desefio sorteador de números

--- sorteador-numeros/app.js
+++ sorteador-numeros/app.js
@@ -17,9 +17,31 @@ function sortear(){
         sorteados.push(numero);
     }
     
-    alert(`Números sorteados: ${sorteados}`);
+    let resultado = document.getElementById('resultado');
+    resultado.innerHTML = `<label class="texto__paragrafo">Números sorteados: ${sorteados}</label>`;
+    alterarStatusBotao();
 }
+
 
 function obterNumeroAleatorio(min, max){
     return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function reiniciar(){
+    document.getElementById('quantidade').value = '';
+    document.getElementById('de').value = '';
+    document.getElementById('ate').value = '';
+    document.getElementById('resultado').innerHTML = '<label class="texto__paragrafo">Números sorteados:  nenhum até agora</label>';
+    alterarStatusBotao();
+}
+
+function alterarStatusBotao(){
+    let botao = document.getElementById('btn-reiniciar');
+    if(botao.classList.contains('container__botao-desabilitado')){
+        botao.classList.remove('container__botao-desabilitado');
+        botao.classList.add('container__botao');
+    }else{
+        botao.classList.remove('container__botao');
+        botao.classList.add('container__botao-desabilitado');
+    }
 }
